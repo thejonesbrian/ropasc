c949936c3574e3eebd423dce4475bc59de7bceba
brijones.nz@gmail.com <brijones.nz@gmail.com>
2025-11-18 16:29:43 +1300

    logic for playRound in RPS

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return 'rock';
+    } else if (randomNumber === 1) {
+        return 'paper';
+    } else if (randomNumber === 2) {
+        return 'scissors';
+    }
+}
+function getHumanChoice() {
+    return prompt("Enter your choice (rock/paper/scissors): ");
+}
+function playRound(humanChoice, computerChoice) {
+    //draw instance
+    if (humanChoice === computerChoice) {
+        console.log('Tie');
+    }
+    //human choice of rock
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log('Computer wins!')
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log('You win');
+    }
+    //human choice of paper
+    if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log('Computer wins!')
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log('You win');
+    }
+    //human choice of scissors
+    if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log('You win!')
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log('Computer win');
+    }
+
+}
+let human = getHumanChoice().toLowerCase();
+let computer = getComputerChoice();
