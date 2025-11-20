3f012ba6dee9b526c5f312871eb44034a154e236
brijones.nz@gmail.com <brijones.nz@gmail.com>
2025-11-21 10:26:38 +1300

    updating project as per rules on site

--- app.js
+++ app.js
@@ -1,44 +1,66 @@
-console.log("Hello World");
+console.log("Hello Rock Paper Scissors");
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
+    const pcChoice = '';
     if (randomNumber === 0) {
-        return 'rock';
+        pcChoice = 'rock'
     } else if (randomNumber === 1) {
-        return 'paper';
+        pcChoice = 'paper';
     } else if (randomNumber === 2) {
-        return 'scissors';
+        pcChoice = 'scissors';
     }
+    //console.log(pcChoice)
+    return pcChoice;
 }
 function getHumanChoice() {
-    return prompt("Enter your choice (rock/paper/scissors): ");
+    const choice = prompt("Enter your choice (rock/paper/scissors): ");
+    //console.log(choice)
+    return choice;
 }
 function playRound(humanChoice, computerChoice) {
+    console.log(`I choose: ${humanChoice} PC choose: ${computerChoice}`);
     //draw instance
     if (humanChoice === computerChoice) {
         console.log('Tie');
     }
     //human choice of rock
     if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
         console.log('Computer wins!')
     } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
         console.log('You win');
     }
     //human choice of paper
     if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
         console.log('Computer wins!')
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
         console.log('You win');
     }
     //human choice of scissors
     if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log('You win!')
+        humanScore++;
+        console.log('Computer wins!')
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log('Computer win');
+        computerScore++;
+        console.log('You win');
     }
 
 }
-let human = getHumanChoice().toLowerCase();
-let computer = getComputerChoice();
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        let human = getHumanChoice().toLowerCase();
+        let computer = getComputerChoice();
+        playRound(human, computer);
+    }
+    console.log(`Human: ${humanScore} VS Computer: ${computerScore}`);
+}
+
+playGame();
+
