b1afe447edab65437aa99d7039a885a966e6ab22
brijones.nz@gmail.com <brijones.nz@gmail.com>
2026-01-26 00:35:51 +1300

    update button and cleared old comment v2

--- ex.js
+++ ex.js
@@ -30,12 +30,3 @@ btn.addEventListener("click", () => {
 });
 
 
-// // adds the indicated style rule to the element in the div variable
-// div.style.color = "blue";
-
-// // adds several style rules
-// div.style.cssText = "color: blue; background: white;";
-
-// // adds several style rules
-// div.setAttribute("style", "color: blue; background: white;");
-
